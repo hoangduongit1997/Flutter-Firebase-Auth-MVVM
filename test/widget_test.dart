028a8b4fe59ca7143b3fbebee363e4c476960b5f
Hoàng Dương <hoangduongit1997@gmail.com>
2020-09-12 14:54:20 +0700

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth_mvvm/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:yourappname/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
